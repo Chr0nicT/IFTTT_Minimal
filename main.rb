539f3702fd44ed871f77863550104c3de6013ac5
Tanay Findley <46724628+Chr0nicT@users.noreply.github.com>
2019-07-31 17:46:49 -0400

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,8 @@
+require_relative "IFTTT_Minimal"
+
+
+IFTTT_Minimal.new("KEY", "EVENT")
+IFTTT_Minimal.var1 = "Variable 1"
+IFTTT_Minimal.var2 = "Variable 2"
+IFTTT_Minimal.var3 = "Variable 3"
+IFTTT_Minimal.fire
